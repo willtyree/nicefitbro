c24f4c8d3196833fccbe41c24a33f9685d1bfa39
Will Tyree <willtyree.wt@gmail.com>
2023-02-15 22:03:50 -0600

    add local and s3 ingestors

---
+++ src/nicefitbro/data_prep/ingestors/local_ingestor.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from nicefitbro.data_prep.ingestors.ingestors_abc import DataIngestor
+
+class LocalFileIngestor(DataIngestor):
+    """
+    Concrete implementation of the DataIngestor abstract class for importing data from a local file.
+    
+    This class implements the ingest_data method for importing data from a local file using the pandas read_csv function.
+    
+    Attributes:
+        None
+        
+    Methods:
+        ingest_data(file_path):
+            Imports data from a local file.
+            - file_path: string indicating the path to the local file.
+            Returns: pandas DataFrame containing the imported data.
+    """
+    def ingest_data(self, file_path, drop_cols=["Unnamed: 0", "api"]):
+        df = pd.read_csv(file_path)
+
+        if drop_cols:
+            for col in drop_cols:
+                if col in df.columns:
+                    df.drop(columns=[col], axis=1, inplace=True)
+        return df
---
+++ src/nicefitbro/data_prep/ingestors/s3_ingestor.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from io import StringIO
+from nicefitbro.data_prep.ingestors.ingestors_abc import DataIngestor
+
+class S3Ingestor(DataIngestor):
+    """
+    Concrete implementation of the DataIngestor abstract class for importing data from an S3 bucket.
+    
+    This class implements the ingest_data method for importing data from an S3 bucket using the boto3 library.
+    
+    Attributes:
+        None
+        
+    Methods:
+        ingest_data(client, bucket_name, file_name):
+            Imports data from an S3 bucket.
+            - client: an instantiated s3 client
+            - bucket_name: string indicating the name of the S3 bucket.
+            - file_name: string indicating the name of the file in the S3 bucket.
+            Returns: pandas DataFrame containing the imported data.
+    """
+    def ingest_data(self, client, bucket_name, file_name, drop_cols=["Unnamed: 0", "api"]):
+        csv_obj = client.get_object(
+            Bucket=bucket_name,
+            Key=file_name
+        )
+        body = csv_obj['Body']
+        csv_string = body.read().decode('utf-8')
+        df = pd.read_csv(StringIO(csv_string))
+
+        if drop_cols:
+            for col in drop_cols:
+                if col in df.columns:
+                    df.drop(columns=[col], axis=1, inplace=True)
+        return df
